83947a820d794a21049d2b0f779e6e0884b04c52
soak0205 <soak0205@gmail.com>
2015-04-18 14:16:08 -0400

    commited with comments for the functions

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,8 @@
 
 ## Write a short comment describing this function
 
+# makeCacheMatrix function creates a special matrix that can store or cache itself and its inverse matrix once it is calculated and stored
+# the function assumes that the matrix sent in as argument is an invertible matrix and does not do any validations for the same
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
@@ -19,6 +21,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+
+#cacheSolve function computes the inverse of the special matrix created by the above function of makeCacheMatrix
+# the cacheSolve checks if the special matrix has the inverse cached and if yes returns the cached inverse
+# if no cached inverse if found, the function computes the inverse, sets it in the special matrix and returns the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
